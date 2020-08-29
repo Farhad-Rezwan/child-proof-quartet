fe598b67c6ff0631adbae1159c3afb654f8079c3
farhad-rezwan <farhad.rezwan@gmail.com>
2020-08-30 08:57:53 +1000

    Adding Weather API into the application

--- Kids Safe Heaven/Kids Safe Heaven/Controller/HomeScreenViewController.swift
+++ Kids Safe Heaven/Kids Safe Heaven/Controller/HomeScreenViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
+import Foundation
 
-class HomeScreenViewController: UIViewController {
+class HomeScreenViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var conditionLabel: UILabel!
@@ -17,11 +22,70 @@ class HomeScreenViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     
     let gradientLayer = CAGradientLayer()
+    let APIKEY = "8ee001a3144d114f170986b983bd0f9c"
+    var lat = 11.21231231
+    var lon = 13.12121212
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame,type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+//        activityIndicator.startAnimating()
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        
+        if(CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
 
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(APIKEY)&units=metric").responseJSON { (response) in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                
+                
+                
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImageView.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEE"
+                self.dayLabel.text = dateFormatter.string(for: date)
+                
+                
+                let suffix = iconName.suffix(1)
+                if suffix == "n" {
+                    self.setGrayGradientBackground()
+                } else {
+                    self.setBlueGradientBackground()
+                }
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
     }
     
     override func viewWillAppear(_ animated: Bool) {
---
+++ Kids Safe Heaven/Pods/Target Support Files/Pods-Kids Safe Heaven/Pods-Kids Safe Heaven-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kids_Safe_Heaven : NSObject
+@end
+@implementation PodsDummy_Pods_Kids_Safe_Heaven
+@end

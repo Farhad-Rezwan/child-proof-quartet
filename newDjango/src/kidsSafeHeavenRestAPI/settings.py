89adf25dd02da3afe5ee32f3b662ecef427f3d2f
farhad-rezwan <farhad.rezwan@gmail.com>
2020-09-11 21:08:28 +1000

    Before deploying

--- newDjango/src/kidsSafeHeavenRestAPI/settings.py
+++ newDjango/src/kidsSafeHeavenRestAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'li_x^!6&c*cjq$+0-k_b+(^&=#8q6h359!wsd+yi&@&lc0413m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

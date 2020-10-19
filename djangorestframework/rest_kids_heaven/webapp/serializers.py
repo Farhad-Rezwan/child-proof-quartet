05fbdf809fcd00265f38d51e4d98dfd675fa653a
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2020-10-20 09:40:48 +1100

    Deleted unnecessary backend folders, SSH keys for security, before handoverdelevery in master

--- djangorestframework/rest_kids_heaven/webapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . models import park
-
-
-class parkSerializers(serializers.ModelSerializer):
-
-    class Meta:
-        model = park
-        fields = '__all__'
--- newDjango/src/parkapi/serializers.py
+++ newDjango/src/parkapi/serializers.py
@@ -3,8 +3,11 @@ from .models import Park
 
 
 class ParkSerializer(serializers.ModelSerializer):
-    distance = serializers.DecimalField(source='distance.mi', max_digits=10, decimal_places=2, required=False, read_only=True)
+    distance = serializers.DecimalField(
+        source='distance.mi', max_digits=10, decimal_places=2, required=False, read_only=True)
+
     class Meta:
         model = Park
-        fields = ('id', 'park_name', 'park_facility', 'park_equipment', 'park_score', 'location', 'distance')
+        fields = ('id', 'park_name', 'park_facility',
+                  'park_equipment', 'park_score', 'location', 'distance')
         read_only_fields = ('distance',)
